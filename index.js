f3edd5017a566f9d42549947c8be169fae5d11fc
Oliver Lai <oliverlai55@gmail.com>
2018-09-05 16:59:51 -0400

    Refactor post to async await

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ const mailchimp = new Mailchimp(mc_api_key);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Dealing with CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // routes
 app.get('/api/email/members', (req, res) => {
   mailchimp.get(`/lists/${list_id}/members`)
@@ -24,24 +31,49 @@ app.get('/api/email/members', (req, res) => {
     });
 });
 
-app.post('/api/email/signup', (req, res) => {
-  console.log(req.body);
-  console.log(req.body.FIRSTNAME);
-  mailchimp.post(`/lists/${list_id}/members`, {
-    email_address: req.body.email_address,
-    status: 'subscribed',
-    'merge_fields': {
-      "FIRSTNAME": req.body.firstName,
-      'LASTNAME': req.body.lastName
-    }
-  })
-  .then(results => {
-    res.send(results);
-  })
-  .catch(err => {
-    res.status(400).json(err);
-  });
+// app.get('/api/email/members', async (res) => {
+//   try {
+//     const results = await mailchimp.get(`/lists/${list_id}/members`)
+//     res.send(results);
+//   } catch (err) {
+//     res.send(err);
+//   }
+// })
 
+//BEFORE
+// app.post('/api/email/signup', (req, res) => {
+//   mailchimp.post(`/lists/${list_id}/members`, {
+//     email_address: req.body.email_address,
+//     status: 'subscribed',
+//     'merge_fields': {
+//       "FIRSTNAME": req.body.firstName,
+//       'LASTNAME': req.body.lastName
+//     }
+//   })
+//   .then(results => {
+//     res.send(results);
+//   })
+//   .catch(err => {
+//     res.status(400).json(err);
+//   });
+// })
+
+// AFTER
+app.post('/api/email/signup', async (req, res) => {
+  try {
+    const results = await mailchimp.post(`/lists/${list_id}/members`, {
+      email_address: req.body.email_address,
+      status: 'subscribed',
+      'merge_fields': {
+        "FIRSTNAME": req.body.firstName,
+        'LASTNAME': req.body.lastName
+      }
+    });
+  
+    res.send(results);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 })
 
 app.get('/', (req, res) => {
@@ -52,3 +84,4 @@ const port = process.env.PORT || 9001;
 app.listen(port);
 
 console.log(`express app listening on port ${port}`);
+
