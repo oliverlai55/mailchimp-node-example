92d4e24f82222ffbaa2942d7677d79b56a5fdbd7
Oliver Lai <oliverlai55@gmail.com>
2018-09-05 13:26:49 -0400

    Init commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const Mailchimp = require('mailchimp-api-v3');
+require('dotenv').config();
+
+const mc_api_key = process.env.MAILCHIMP_API_KEY;
+const list_id = process.env.MAILING_LIST_ID;
+
+const app = express();
+const mailchimp = new Mailchimp(mc_api_key);
+
+// Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// routes
+app.get('/api/memberList', (req, res) => {
+  mailchimp.get(`/lists/${list_id}/members`)
+    .then(results => {
+      res.send(results);
+    })
+    .catch(err => {
+      res.send(err);
+    });
+});
+
+app.post('/sign-up', (req, res) => {
+  mailchimp.post(`/lists/${list_id}/members`, {
+    email_address: req.body.email_address,
+    status: 'subscribed',
+    'merge_fields': {
+      "FIRSTNAME": req.body.fn,
+      'LASTNAME': req.body.ln
+    }
+  })
+  .then(results => {
+    res.send(results);
+  })
+  .catch(err => {
+    res.status(400).json(err);
+  });
+
+})
+
+const port = process.env.PORT || 9001;
+app.listen(port);
+
+console.log(`express app listening on port ${port}`);
